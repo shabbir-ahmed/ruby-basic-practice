1678adffe48fcc87c3e072f5930859140eaf1e21
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-11-04 17:33:52 +0000

    Ruby basic practices

---
+++ playground.rb
@@ -0,0 +1,8 @@
+puts "Hello enter your first name"
+
+first_name = gets.chomp
+puts "Hello enter your last name"
+
+last_name = gets.chomp
+
+puts "Welcome #{first_name} #{last_name}, to the playground!"
