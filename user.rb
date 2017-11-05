6f8a52d8ceef80bc027448fa3501fe9648854c57
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-11-05 12:01:49 +0000

    Module

--- user.rb
+++ user.rb
@@ -1,4 +1,12 @@
+module Destructable
+    def destroy(myname)
+        puts "I will destroy the object"
+    end
+end
+
 class User
+    include Destructable
+    
     attr_accessor :name, :email
     
     def initialize(name, email)
@@ -8,6 +16,10 @@ class User
     
     def run
         puts "Hey I'm running"
+    end
+    
+    def self.identify_yourself
+        puts "Hey im a class method"
     end
     
     def get_name
@@ -20,7 +32,9 @@ class User
 end
 # Inheritence
 class Buyer < User
-    
+    def run
+        puts "Hey I'm not running and I'm an buyer class"
+    end
 end
 
 class Seller < User
@@ -39,3 +53,15 @@ puts "My new username is #{user.name} and my email is #{user.email}"
 
 buyer1 = Buyer.new("John Doe", "john@example.com")
 buyer1.run
+
+seller1 = Seller.new("John Doe1", "john1@example.com")
+seller1.run
+
+admin1 = Admin.new("John Doe2", "john2@example.com")
+admin1.run
+
+puts Buyer.ancestors
+
+User.identify_yourself
+
+user.destroy("myname")
